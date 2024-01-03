8d78e0a926b8539f12b624d44379393a2d098540
Nguyễn Ngọc Quốc <quoc.nn00820@sinhvien.hoasen.edu.vn>
2024-01-03 17:29:50 +0700

    Update [About Us, Services, Service Single, Pricing UI/UX]

--- source-basic/js/main.js
+++ source-basic/js/main.js
@@ -1,25 +1,26 @@
 $(document).ready(function () {
-    $(window).scroll(function () { 
+    $(window).scroll(function () {
         console.log($(window).scrollTop());
-        if($(window).scrollTop() > 200){
+        if ($(window).scrollTop() > 200) {
             $(".search").addClass("sticky");
-        }else{
+        } else {
             $(".search").removeClass("sticky");
         }
     });
 });
 
-
 function changeColor(element) {
     // Lấy tất cả các phần tử có lớp "areaRoom-design-item"
-    var items = document.querySelectorAll('.areaRoom-design-item');
-  
+    var items = document.querySelectorAll(".areaRoom-design-item");
+
     // Loại bỏ lớp "selected" từ tất cả các phần tử
-    items.forEach(function(item) {
-      item.classList.remove('selected');
+    items.forEach(function (item) {
+        item.classList.remove("selected");
+        item.querySelector(".jsBtnSelected").src = "img/icon/btnNextCirle.svg";
     });
-  
+
     // Thêm lớp "selected" cho phần tử được click
-    element.classList.add('selected');
-  }
-  
+    element.classList.add("selected");
+    element.querySelector(".jsBtnSelected").src = "img/icon/btnNextCircleActive.svg";
+}
+
