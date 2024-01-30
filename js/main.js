712d33c80588eda35684c86d2f64e09966dfe6f7
Nguyễn Ngọc Quốc <102974265+NQuocNk20@users.noreply.github.com>
2024-01-30 19:04:39 +0700

    Update [Footer -  Mobile, Home Page - Mobile]

--- js/main.js
+++ js/main.js
@@ -228,39 +228,6 @@
 //     }
 // });
 
-// GASP
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-ScrollTrigger.normalizeScroll(true)
-
-// create the smooth scroller FIRST!
-let smoother = ScrollSmoother.create({
-    smooth: 2,
-    effects: true,
-    normalizeScroll: true
-});
-
-// pin box-c when it reaches the center of the viewport, for 300px
-ScrollTrigger.create({
-    trigger: ".box-c",
-    pin: true,
-    start: "center center",
-    end: "+=300",
-    markers: true
-});
-
-document.querySelector("button").addEventListener("click", e => {
-    // scroll to the spot where .box-c is in the center.
-    // parameters: element, smooth, position
-    smoother.scrollTo(".box-c", true, "center center");
-
-    // or you could animate the scrollTop:
-    // gsap.to(smoother, {
-    //  scrollTop: smoother.offset(".box-c", "center center"),
-    //  duration: 1
-    // });
-});
-
 $(document).ready(function () {
     $(window).scroll(function () {
         console.log($(window).scrollTop());
